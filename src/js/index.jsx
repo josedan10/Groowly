4568a0820b183559bd3e1fc238728b6f2477c45a
Jose Daniel <josedanq100@gmail.com>
2018-03-12 11:59:36 -0400

    Quit jQuery

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,19 +3,27 @@ import ReactDOM from 'react-dom';
 // import Nav from './nav/nav';
 import Nav from './nav/nav-menu';
 
-$(document).ready(function () {
 
+// Links para el menu y las redes
+const links = ['Home', 'Services', 'Selected Works', 'Own Community', 'Team', 'Clients', 'Contact'];
+// const redes = ['twitter', 'facebook', 'instagram', 'youtube'];
 
-	// Links para el menu y las redes
-	const links = ['Home', 'Services', 'Selected Works', 'Own Community', 'Team', 'Clients', 'Contact'];
-	// const redes = ['twitter', 'facebook', 'instagram', 'youtube'];
+// Imagen del logo
+const logo = 'src/img/logo.svg'; 					
 
-	// Imagen del logo
-	const logo = 'src/img/logo.svg'; 					
+ReactDOM.render(
+	<Nav tipo='nav-left' logo={logo} links={links} />,
+	document.getElementById('nav')
+);
 
-	ReactDOM.render(
-		<Nav tipo='nav-left' logo={logo} links={links} />,
-		document.getElementById('nav')
-	);
-});
+/* <aside class="left">
+			<div id="nav"></div>
+		</aside>
+		<aside id="social" class="right">
+			<ul>
+				<li><a href="" target="_blank"><i class="icon icon-facebook"></i></a></li>
+				<li><a href="" target="_blank"><i class="icon icon-twitter"></i></a></li>
+				<li><a href="" target="_blank"><i class="icon icon-instagram"></i></a></li>
+			</ul>
+		</aside> */
 
