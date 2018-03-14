62c9dcf1746410b180b3f7823c4b6961c6992fdf
Jose Daniel <josedanq100@gmail.com>
2018-03-14 16:57:17 -0400

    Work section styles setup

--- src/js/pages/selected-works.jsx
+++ src/js/pages/selected-works.jsx
@@ -10,18 +10,54 @@ export default class SelectedWorks extends React.Component {
 			<div>
 				<section className='selected-works' id='Selected_Works' onTouchMove={this.props.onTouchMove}>
 					<div className='flex-center'>
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
-						<div className='flex-center' />
+						<div className='flex-center'>
+							<img src='src/img/works/Carolinawork.jpg'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/Equilibriowork.jpg'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/Fierawork.jpg'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/Gabrielawork.jpg'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/Kathieswork.jpg'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/Venuswork.jpg'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/'/>
+						</div>
+
+						<div className='flex-center'>
+							<img src='src/img/works/'/>
+						</div>
+
 					</div>
 				</section>
 			</div>
