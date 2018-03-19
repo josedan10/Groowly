a7a4cdc4eb2bed8f976ba7a569656e235e177ff5
Jose Daniel <josedanq100@gmail.com>
2018-03-19 10:17:53 -0400

    Init backend

---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+	<title>Groowly</title>
+
+	<link rel="icon" href="favicon.png" type="image/x-icon" />	
+	<link rel="stylesheet" href="src/css/style.css">
+	<link rel="stylesheet" href="src/css/animate.css">
+	<link rel="stylesheet" href="src/css/custom.css">
+	
+</head>
